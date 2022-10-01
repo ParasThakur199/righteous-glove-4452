a8f29060b946ecee33ee6048acfeedaee8c8ce70
ParasThakur199 <parasjamwal19@gmail.com>
2022-10-01 22:06:15 +0530

    changes

--- script/index.js
+++ script/index.js
@@ -1,3 +1,11 @@
+import { Navbar } from '../components/Navbar.js'
+import { footer } from '../components/footer.js'
+
+let navbar_div = document.getElementById('Navbar')
+navbar_div.innerHTML = Navbar();
+
+let footer_div = document.getElementById('footer')
+footer_div.innerHTML = footer();
 
 let user=JSON.parse(localStorage.getItem('userInfo')) 
 let searchBtn=document.getElementById('search')
